7b40240a1a9320a1343763547a5fb77fef300cba
Asher Moldwin <amoldwin@umd.edu>
2019-11-03 13:50:17 -0500

    added bar plots for absolute and normalized counts

--- QuestionCount/questioncount.py
+++ QuestionCount/questioncount.py
@@ -1,6 +1,12 @@
 
 import re
+import tkinter
+import matplotlib
+matplotlib.use('TkAgg')
 import operator
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 questionCounts = {}
 totalCounts = {}
@@ -35,7 +41,7 @@ sorted_Qcounts = sorted(questionCounts.items(), key=operator.itemgetter(1))
 
 print(sorted_Qcounts)
 print()
-normalized = questionCounts
+normalized = questionCounts.copy()
 for speaker,qCount in questionCounts.items():
         if qCount != 0:
                 normalized[speaker] = normalized[speaker]/float(totalCounts[speaker])
@@ -43,3 +49,34 @@ for speaker,qCount in questionCounts.items():
 sorted_Ncounts = sorted(normalized.items(), key=operator.itemgetter(1))
 
 print(sorted_Ncounts)
+
+
+objects = ('Kirk','McCoy', 'Spock','Scott', 'Sulu','Uhura','Chekov','Pike','Chapel')
+freqs =  [questionCounts[a.upper()] for a in objects] 
+normfreqs =  [normalized[a.upper()] for a in objects] 
+
+ypos = np.arange(len(objects))
+print(freqs,ypos)
+plt.bar(ypos,freqs,align='center',alpha =0.5)
+plt.xticks(ypos,objects)
+plt.ylabel('Number of Questions Asked by Character')
+plt.xlabel('Top Characters')
+plt.title('Frequency of questions per character in Star Trek')
+plt.savefig('absoluteFreqs.png')
+plt.show()
+
+
+#plot normalized question frequencies
+plt.bar(ypos,normfreqs,align='center',alpha =0.5)
+plt.xticks(ypos,objects)
+plt.ylabel('Number of Questions, Divided by Character\'s Total Lines')
+plt.xlabel('Top Characters')
+plt.title('Normalized Frequency of questions per character in Star Trek')
+
+#plt.show()
+plt.savefig('normalizedFreqs.png')
+
+
+
+
+
